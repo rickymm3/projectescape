31f2a7e5741aca5bc28ace8ed398cc3d94f1d237
rickymm3 <rickymm3@gmail.com>
2017-06-12 15:24:50 -0400

    impression fix

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -8,13 +8,15 @@ class ImpressionsController < ApplicationController
     require 'gruff'
     g = Gruff::Line.new
     g.title = 'Impression chart'
-    i1 = Impression.where('created_at >= ?', 1.day.ago).count
-    i2 = Impression.where('created_at >= ?', 2.day.ago).count
-    i3 = Impression.where('created_at >= ?', 3.day.ago).count
-    i4 = Impression.where('created_at >= ?', 4.day.ago).count
-    i5 = Impression.where('created_at >= ?', 5.day.ago).count
-    i6 = Impression.where('created_at >= ?', 6.day.ago).count
-    i7 = Impression.where('created_at >= ?', 7.day.ago).count
+    day = Time.now.beginning_of_day
+    i8 = Impression.where(:created_at => Time.now..day).count
+    i7 = Impression.where(:created_at => 1.day.ago.beginning_of_day..day).count
+    i6 = Impression.where(:created_at => 2.day.ago.beginning_of_day..1.day.ago.beginning_of_day).count
+    i5 = Impression.where(:created_at => 3.day.ago.beginning_of_day..2.day.ago.beginning_of_day).count
+    i4 = Impression.where(:created_at => 4.day.ago.beginning_of_day..3.day.ago.beginning_of_day).count
+    i3 = Impression.where(:created_at => 5.day.ago.beginning_of_day..4.day.ago.beginning_of_day).count
+    i2 = Impression.where(:created_at => 6.day.ago.beginning_of_day..5.day.ago.beginning_of_day).count
+    i1 = Impression.where(:created_at => 7.day.ago.beginning_of_day..6.day.ago.beginning_of_day).count
 
     g.labels = {
                  0 => 7.days.ago.strftime("%b %d"),
@@ -23,9 +25,10 @@ class ImpressionsController < ApplicationController
                  3 => 4.days.ago.strftime("%b %d"),
                  4 => 3.days.ago.strftime("%b %d"),
                  5 => 2.days.ago.strftime("%b %d"),
-                 6 => 1.days.ago.strftime("%b %d")
+                 6 => 1.days.ago.strftime("%b %d"),
+                 7 => "Today"
                 }
-    g.data :impressions, [i1, i2, i3, i4, i5, i6, i7]
+    g.data :impressions, [i1, i2, i3, i4, i5, i6, i7, i8]
 
     send_data g.to_blob,
               :disposition => 'inline',
