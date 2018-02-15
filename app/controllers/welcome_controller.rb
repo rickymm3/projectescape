b4f8d5d952e9df71016e7fe24f7b6a629b9a3455
Ricky Mason <rickymm3@gmail.com>
2018-02-15 11:12:51 -0500

    new title homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   impressionist :actions=>[:index],:unique => [:session_hash]
   def index
     @posts = Post.limit(3).order('id desc')
+    @title = "The Philadelphia Area's BEST Escape Room! "
   end
 
   def assign_title
